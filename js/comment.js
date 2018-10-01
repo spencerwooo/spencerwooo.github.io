82b63618bd262bde345cd0c79a99e9eeebe28119
spencerwoo98 <spencerwoo98@foxmail.com>
2018-10-01 16:53:05 +0800

    Site updated: 2018-10-01 16:52:52

--- js/comment.js
+++ js/comment.js
@@ -3799,7 +3799,7 @@ var Gitment =
                     }, options);
 
                     this.state.user.isLoggingIn = true;
-                    _utils.http.post('https://gh-oauth.imsun.net', {
+                    _utils.http.post('https://github.com/login/oauth/access_token', {
                         code: code,
                         client_id: client_id,
                         client_secret: client_secret
